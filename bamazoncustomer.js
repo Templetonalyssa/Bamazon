9ccdd97df9312d1ce6cb5b4ba02c13ed1794df2d
Alyssa Templeton <templetonalyssa@gmail.com>
2018-11-10 17:11:51 -0500

    Working with price update

--- bamazoncustomer.js
+++ bamazoncustomer.js
@@ -61,49 +61,43 @@ function start() {
             // based on their answer, either call the bid or the post functions
             console.log("Purchase " + answer.unitChoice + " units of product ID " + answer.idChoice);
 
-            
+
             checkRequest(parseInt(answer.unitChoice), parseInt(answer.idChoice));
-            //connection.end();
+
         });
 
 }
 
-function checkRequest (unit, id) {
+//checkRequest is similiar to the second half of the bidAuction function from Great Bay example 10. If we wanted to we could also make function start() include the if/else statement from checkRequest to make into one big function, just like Great Bay example 10.
+function checkRequest(unit, id) {
     // Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
-          console.log(unit);
-     connection.query("SELECT stock_quantity FROM products WHERE ?" , {item_id:id}, function(err, result){
+    connection.query("SELECT stock_quantity, price FROM products WHERE ?", {
+        item_id: id
+    }, function (err, result) {
 
         if (result[0].stock_quantity >= unit) {
             // bid was high enough, so update db, let the user know, and start over
             connection.query(
-              "UPDATE products SET ? WHERE ?",
-              [
-                {
-                  stock_quantity: result[0].stock_quantity - unit
-                },
-                {
-                  item_id: id
+                "UPDATE products SET ? WHERE ?",
+                [{
+                        stock_quantity: result[0].stock_quantity - unit
+                    },
+                    {
+                        item_id: id
+                    }
+                ],
+                function (error) {
+                    if (error) throw error;
+                    console.log("Item purchased successfully! Total cost of purchase is " + unit*(result[0].price));
+                    start();
                 }
-              ],
-              function(error) {
-                if (error) throw error;
-                console.log("Item purchased successfully!");
-                start();
-              }
             );
-          }
-          else {
+        } else {
             // bid wasn't high enough, so apologize and start over
             console.log("We do not have that many items in stock. Choose a lower quantity...");
             start();
-          }
+        }
 
-     })
+    })
 
-    // If not, the app should log a phrase like Insufficient quantity!, and then prevent the order from going through.
-    // However, if your store does have enough of the product, you should fulfill the customer's order.
-    
-    // This means updating the SQL database to reflect the remaining quantity.
-    // Once the update goes through, show the customer the total cost of their purchase.
-    
 }
